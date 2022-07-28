569b558725a2e1b3b4507070da073c6cdd9583d5
payal-s-1 <M1089106@mindtree.com>
2022-07-28 13:18:59 +0530

    pushing

---
+++ src/main/java/com/mindtree/herokuapp/App.java
@@ -0,0 +1,17 @@
+package com.mindtree.herokuapp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://the-internet.herokuapp.com/");
+
+	}
+
+}
